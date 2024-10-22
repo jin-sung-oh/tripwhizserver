ed6994281ee0f4708ae156c43883de858472aa82
USER <ckalswn371004@gmail.com>
2024-10-22 14:33:44 +0900

    fileUploadTest_MJ--zerock

--- src/main/java/com/example/demo/faq/controller/FAQController.java
+++ src/main/java/com/example/demo/faq/controller/FAQController.java
@@ -3,8 +3,10 @@ package com.example.demo.faq.controller;
 import com.example.demo.common.dto.PageRequestDTO;
 import com.example.demo.common.dto.PageResponseDTO;
 import com.example.demo.faq.dto.FAQListDTO;
+import com.example.demo.faq.service.FAQService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class FAQController {
 
+    @Autowired
     private final FAQService faqService;
 
     @GetMapping("list")
