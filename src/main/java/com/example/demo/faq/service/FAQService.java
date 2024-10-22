a4c909f8ab0da8b2ebdcb1aee4f88d5d9e5f92ac
eggzuxi <eggzuxi99@gmail.com>
2024-10-22 11:05:01 +0900

    beforeListTest_JH

--- src/main/java/com/example/demo/faq/service/FAQService.java
+++ src/main/java/com/example/demo/faq/service/FAQService.java
@@ -1,10 +1,21 @@
 package com.example.demo.faq.service;
 
+import com.example.demo.common.dto.PageRequestDTO;
+import com.example.demo.common.dto.PageResponseDTO;
+import com.example.demo.faq.domain.FAQEntity;
+import com.example.demo.faq.dto.FAQListDTO;
 import com.example.demo.faq.repository.FAQRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -14,6 +25,36 @@ public class FAQService {
 
     private final FAQRepository faqRepository;
 
+    @Transactional
+    public PageResponseDTO<FAQListDTO> list(PageRequestDTO pageRequestDTO) {
 
+        Pageable pageable = PageRequest.of(pageRequestDTO.getPage() -1,
+                pageRequestDTO.getSize(),
+                Sort.by("fno").descending());
+
+        Page<FAQEntity> result = faqRepository.findAll(pageable);
+
+        List<FAQListDTO> dtoList = result.get().map(FAQEntity -> {
+
+            FAQListDTO dto = FAQListDTO.builder()
+                    .fno(FAQEntity.getFno())
+                    .question(FAQEntity.getQuestion())
+                    .viewCnt(FAQEntity.getViewCnt())
+                    .delFlag(false)
+                    .build();
+
+            return dto;
+
+        }).collect(Collectors.toUnmodifiableList());
+
+        long total = result.getTotalElements();
+
+        return PageResponseDTO.<FAQListDTO>withAll()
+                .dtoList(dtoList)
+                .pageRequestDTO(pageRequestDTO)
+                .totalCount(total)
+                .build();
+
+    }
 
 }
