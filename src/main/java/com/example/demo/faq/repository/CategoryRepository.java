e2e3fb18cfaee840ddd74e9a3d57015cfedfd418
eggzuxi <eggzuxi99@gmail.com>
2024-10-22 09:26:37 +0900

    addPageDTO_JH

--- src/main/java/com/example/demo/faq/repository/CategoryRepository.java
+++ src/main/java/com/example/demo/faq/repository/CategoryRepository.java
@@ -1,6 +1,6 @@
 package com.example.demo.faq.repository;
 
-import com.example.demo.faq.domain.CategoryEntity;
+import com.example.demo.common.domain.CategoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<CategoryEntity,Integer> {
