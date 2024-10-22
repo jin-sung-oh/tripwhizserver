4e995532c2c1a1c5eac36376d03c4384370ef766
eggzuxi <eggzuxi99@gmail.com>
2024-10-22 15:46:46 +0900

    softDeleteController_JH

--- src/main/java/com/example/demo/faq/controller/FAQController.java
+++ src/main/java/com/example/demo/faq/controller/FAQController.java
@@ -7,9 +7,8 @@ import com.example.demo.faq.service.FAQService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @Log4j2
 @RestController
@@ -20,6 +19,7 @@ public class FAQController {
     @Autowired
     private final FAQService faqService;
 
+    // 리스트 조회
     @GetMapping("list")
     public PageResponseDTO<FAQListDTO> list(PageRequestDTO pageRequestDTO) {
 
@@ -27,6 +27,12 @@ public class FAQController {
 
     }
 
-    
+    // 삭제
+    @DeleteMapping("/{fno}")
+    public ResponseEntity<Void> softDeleteFAQ(@PathVariable Long fno) {
+        faqService.softDeleteFAQ(fno);
+        return ResponseEntity.ok().build();
+    }
+
 
 }
