b2dc901a634d0ce78833d4f7075fc6024b7d6811
eggzuxi <eggzuxi99@gmail.com>
2024-10-23 09:31:19 +0900

    forPull_JH

--- src/main/java/com/example/demo/faq/controller/FAQController.java
+++ src/main/java/com/example/demo/faq/controller/FAQController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 @Log4j2
 @RestController
-@RequestMapping("/api/v1/faqs")
+@RequestMapping("/api/faqs")
 @RequiredArgsConstructor
 public class FAQController {
 
@@ -28,7 +28,7 @@ public class FAQController {
     }
 
     // 추가
-    @PostMapping("/add")
+    @PostMapping("")
     public ResponseEntity<Long> addFaq(@RequestBody FAQEntity faq) {
 
         FAQEntity savedFaq = faqService.addFaq(faq);
@@ -36,7 +36,6 @@ public class FAQController {
         return ResponseEntity.status(HttpStatus.CREATED).body(fno); // fno 값을 반환
 
     }
-
 
 
 }
