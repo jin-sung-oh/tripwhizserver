6f96740826f37db1e802429991fee54b3b656b89
진성오 <wlsqoren@naver.com>
2024-10-24 14:17:22 +0900

    addWebConfig_so

--- src/main/java/com/example/demo/faq/controller/FAQController.java
+++ src/main/java/com/example/demo/faq/controller/FAQController.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/faqs")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "*")
 public class FAQController {
 
     private final FAQService faqService;
