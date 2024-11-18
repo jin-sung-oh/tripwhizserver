a0d4addf06c76eaa06be38b7e7299b838fea70ee
eggzuxi <eggzuxi99@gmail.com>
2024-11-18 09:43:11 +0900

    categoryAdd_JH

--- src/main/java/com/example/demo/category/dto/CategoryDTO.java
+++ src/main/java/com/example/demo/category/dto/CategoryDTO.java
@@ -2,7 +2,7 @@ package com.example.demo.category.dto;
 
 
 import com.example.demo.category.domain.Category;
-import jakarta.validation.constraints.NotNull;
+import com.example.demo.category.domain.ParentCategory;
 import lombok.Data;
 
 @Data
@@ -10,12 +10,12 @@ public class CategoryDTO {
 
     private Long cno;
 
-
+    private ParentCategory category;
 
     public static CategoryDTO fromEntity(Category category) {
         CategoryDTO dto = new CategoryDTO();
         dto.setCno(category.getCno());
-
+        dto.setCategory(category.getCategory());
         return dto;
     }
 
