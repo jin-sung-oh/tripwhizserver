4e26a664f0672dc318d59da612dffbcaa7861277
Seola CHOE <tjfdk0493@gmail.com>
2024-11-27 16:04:20 +0900

    Nationality REST-ing //1127_SA

---
+++ src/main/java/com/example/demo/nationality/repository/NationalityRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.nationality.repository;
+
+import com.example.demo.nationality.domain.Nationality;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NationalityRepository extends JpaRepository<Nationality, Long> {
+}
