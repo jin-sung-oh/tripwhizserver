50df67d399295eaac86d0b6772abe2b1743e8433
eggzuxi <eggzuxi99@gmail.com>
2024-12-02 01:22:02 +0900

    trashCode_JH

--- src/main/java/com/example/demo/category/domain/ParentCategory.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.category.domain;
-
-public enum ParentCategory {
-    STORAGE,    // 수납/편의
-    CLOTHING,   // 의류
-    SAFETY,     // 안전/위생
-    ACCESSORY,  // 악세사리
-    ACTIVITY    // 액티비티 용품
-}
--- src/main/java/com/example/demo/category/domain/ThemeCategory.java
+++
@@ -1,5 +0,0 @@
-//package com.example.demo.category.domain;
-//
-//public enum ThemeCategory {
-//    RELAXATION, BUSINESS, GASTRONOMY, SHOPPING, ACTIVITY, CULTURE
-//}
